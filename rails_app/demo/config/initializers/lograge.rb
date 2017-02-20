4263048d029390350c202c12fc9538c79efad3d9
gaga5lala <gaga5lala1013@gmail.com>
2017-02-20 14:41:01 +0800

    feat(rails/app): Add custom format

--- rails_app/demo/config/initializers/lograge.rb
+++ rails_app/demo/config/initializers/lograge.rb
@@ -3,4 +3,15 @@ Demo::Application.configure do
 
   config.lograge.keep_original_rails_log = false
   config.lograge.logger = ActiveSupport::Logger.new(STDOUT)
+
+  config.lograge.custom_options = lambda do |event|
+    {
+      host: event.payload[:host],
+      remote_ip: event.payload[:remote_ip],
+      origin: event.payload[:origin],
+      uuid: event.payload[:uuid],
+      type: 'request',
+      params: event.payload[:params]
+    }
+  end
 end
