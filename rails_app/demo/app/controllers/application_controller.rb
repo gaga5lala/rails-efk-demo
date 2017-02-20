dc21d0bc088fe3e36cc351782a9e984e78b593bc
gaga5lala <gaga5lala1013@gmail.com>
2017-02-20 14:51:04 +0800

    feat(rails/app): Support custom event payload

--- rails_app/demo/app/controllers/application_controller.rb
+++ rails_app/demo/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def append_info_to_payload(payload)
+    super
+    payload[:host] = "asdfasdf"# request.host
+    payload[:host] = request.host
+    payload[:remote_ip] = request.remote_ip
+    payload[:origin] = request.headers['HTTP_ORIGIN'] rescue '' + request.headers['ORIGINAL_FULLPATH']
+    payload[:uuid] =  request.uuid
+    payload[:browser] = "#{browser.name} #{browser.full_version}"
+    payload[:user_agent] = request.headers['HTTP_USER_AGENT']
+  end
 end
